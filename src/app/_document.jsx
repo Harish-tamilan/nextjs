77dc0938df50edb77c7e519a77b1c6d10b16ac3d
Harish Kumar Rajasekaran <harishkumarrajasekaran@Harishs-MacBook-Pro.local>
2024-06-01 23:20:19 +0530

    Removed styled-components

--- src/app/_document.jsx
+++ src/app/_document.jsx
@@ -1,16 +1,11 @@
 import Document from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
   static async getInitialProps (ctx) {
-    const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
 
     try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
-        })
+     
 
       const initialProps = await Document.getInitialProps(ctx)
       return {
@@ -18,12 +13,10 @@ export default class MyDocument extends Document {
         styles: (
           <>
             {initialProps.styles}
-            {sheet.getStyleElement()}
           </>
         )
       }
     } finally {
-      sheet.seal()
     }
   }
 }
